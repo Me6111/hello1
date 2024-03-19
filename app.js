4563dd04bea4e6124adec56ffe17dbd090394b67
Me6111 <maksymmpawlowski@gmail.com>
2024-03-19 19:33:26 +0000

    Serve static files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('<body style="background-color:black;"><p style="color:darkgreen;">Hello</p></body>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
