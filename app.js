6d8676586cd79af747eb1cc1607e3e87d6ea87f1
Me6111 <maksymmpawlowski@gmail.com>
2024-03-19 20:57:48 +0000

    Updated app.js to handle CORS

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require('cors'); // Require cors middleware
 const app = express();
 const path = require('path');
 
+app.use(cors()); // Use cors middleware
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Add this new route
+app.get('/hello', (req, res) => {
+    res.send('hello frontend');
 });
 
 const port = process.env.PORT || 3000;
