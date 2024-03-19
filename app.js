19c91f82fce0bdb0cc6e821c1b7161fcc1681df0
Me6111 <maksymmpawlowski@gmail.com>
2024-03-19 20:37:53 +0000

    Add hello route

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Add this new route
+app.get('/hello', (req, res) => {
+    res.send('hello frontend');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
