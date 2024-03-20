cc4737453af3abdfcaa34664c9ba8cfdb49e4b00
Me6111 <maksymmpawlowski@gmail.com>
2024-03-20 23:30:39 +0000

    'change-01'

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const cors = require('cors'); // Require cors middleware
 const app = express();
 const path = require('path');
+const { Pool } = require('pg'); // Require pg module
+
+// Create a new pool using the DATABASE_URL environment variable
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 app.use(cors()); // Use cors middleware
 app.use(express.static('public'));
@@ -24,7 +33,7 @@ app.get('/countries', async (req, res) => {
       console.error(err);
       res.status(500).send('Error occurred');
     }
-  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`)); 
