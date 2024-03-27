76ed69aae2d98e07fa1a7205760dca156fb48eb6
Me6111 <maksymmpawlowski@gmail.com>
2024-03-27 10:58:17 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/countries', async (req, res) => {
     const country = req.body.country;
     console.log(`Received country from client: ${country}`);
     
-    const country_id = await pool.query('SELECT country_id FROM countries WHERE country_name = $1', [country]);
+    const country_id = await pool.query("SELECT country_id FROM countries WHERE country_name = '" + country + "'");
     console.log(country_id);
     res.send(country_id);
   } catch (err) {
